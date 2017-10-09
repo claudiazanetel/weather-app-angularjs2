808644c19ece07563cb0153301ad463905422b14
Your Name <you@example.com>
2017-10-09 13:52:00 +0200

    got data from api openweather and showed them in html

--- src/app/current/current.component.ts
+++ src/app/current/current.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnChanges, Input, SimpleChange } from '@angular/core';
+import { HttpClient, HttpParams} from '@angular/common/http';
 
 @Component({
   selector: 'current',
@@ -7,11 +8,25 @@ import { Component, OnChanges, Input, SimpleChange } from '@angular/core';
 })
 export class CurrentComponent implements OnChanges {
   @Input() cityName : string;
-  constructor() { }
+  currentData: Object;
+  constructor(private http: HttpClient) { 
+    this.currentData;
+  }
 
   ngOnChanges(changes: {[propKey: string]: SimpleChange}) {
     console.log('changed current');
     console.log(changes.cityName.currentValue);
+    if (changes.cityName.currentValue) {
+      this.http.get('http://api.openweathermap.org/data/2.5/weather', {
+        params: new HttpParams()
+          .set('q', changes.cityName.currentValue)
+          .set('units', 'metric')
+          .set('appid', '4b80da9843e489246022d72d79a1e508')
+      }).subscribe(data => {
+        console.log(data);
+        this.currentData = data;
+      });
+    }
   }
 
 }
