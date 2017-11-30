34a75b70da6df5b54b0296f4fbf985765c4fd172
Claudia Zanetel <claudia.zanetel@gmail.com>
2017-11-30 21:01:26 +0100

    first commit

--- src/app/forecast/forecast.component.ts
+++ src/app/forecast/forecast.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnChanges, Input, SimpleChange } from '@angular/core';
 import { HttpClient, HttpParams} from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'forecast',
@@ -11,40 +12,53 @@ export class ForecastComponent implements OnChanges{
   forecastDailyData: Object;
   forecastDetailsHourly: Object;
   selectedDay: number;
-  isEmpty: boolean;
+  isEmpty: boolean = true;
   city: string;
+
   constructor(private http: HttpClient) { 
-    this.forecastDailyData;
-    this.forecastDetailsHourly;
-    this.isEmpty = false;
-    this.city;
-    this.selectedDay;
   }
 
   ngOnChanges(changes: {[propKey: string]: SimpleChange}) {
     console.log(changes.cityName.currentValue);
     this.city = changes.cityName.currentValue;
     if (changes.cityName.currentValue) {
-      this.http.get('http://api.openweathermap.org/data/2.5/forecast/daily', {
-        params: new HttpParams()
-          .set('q', changes.cityName.currentValue)
-          .set('units', 'metric')
-          .set('appid', '4b80da9843e489246022d72d79a1e508')
-      }).subscribe(
+      this.fetchData(changes.cityName.currentValue, "daily")
+      .subscribe(
         data => {
-        console.log(data['list']);
-        this.forecastDailyData = data;
-        this.isEmpty = true;
-        this.getDayDetails(this.forecastDailyData['list'][0]['dt'])
+          console.log(data['list']);
+          this.forecastDailyData = data;
+          this.fetchData(changes.cityName.currentValue, "")
+          .subscribe(
+            data => {
+              this.forecastDetailsHourly = data;
+              console.log(this.forecastDetailsHourly);
+              this.setSelectedDay(this.forecastDailyData['list'][0]['dt']);
+              this.isEmpty = false;
+            },
+            err => console.log('error in details')
+          );
         },
         err => {
-          this.isEmpty = false;
-        });
+          this.isEmpty = true;
+        }
+      );
     }
   }
 
-  getDayDetails(day){
+  private fetchData(city :string, type :string) :Observable<Object> {
+    return this.http.get('http://api.openweathermap.org/data/2.5/forecast/'+type, {
+      params: new HttpParams()
+        .set('q', city)
+        .set('units', 'metric')
+        .set('appid', '4b80da9843e489246022d72d79a1e508')
+    })
+  }
+
+  setSelectedDay(day :number) :void {
     this.selectedDay = day;
+  }
+
+  getDayDetails() :Array<Object> {
     let dateFrom = new Date(this.selectedDay * 1000);
     let dateTo = new Date(this.selectedDay * 1000);
     dateFrom.setHours(0, 0, 0, 0);
@@ -52,36 +66,9 @@ export class ForecastComponent implements OnChanges{
     let from = dateFrom.getTime() / 1000;
     let to = dateTo.getTime() / 1000;
 
-    this.http.get('http://api.openweathermap.org/data/2.5/forecast/', {
-      params: new HttpParams()
-        .set('q', this.city)
-        .set('units', 'metric')
-        .set('appid', '4b80da9843e489246022d72d79a1e508')
-    }).subscribe(
-      data => {
-      function filterDetails(listData) {
-        return listData.filter(function(element) {
-            return element.dt >= from && element.dt < to;
-        })
-      }
-      this.forecastDetailsHourly = filterDetails(data['list']);
-      console.log(this.forecastDetailsHourly);
-      },
-      err => {
-        console.log('error in details')
-      });
+    return this.forecastDetailsHourly['list'].filter(
+      (element) => { return element.dt >= from && element.dt < to }
+    )
   }
 }
 
-// ng-repeat call getDaysDetails
-/* $scope.getDaysDetails = function () {
-  var dateFrom = new Date($scope.selectedDay * 1000);
-  var dateTo = new Date($scope.selectedDay * 1000);
-  dateFrom.setHours(0, 0, 0, 0);
-  dateTo.setHours(23, 59, 59, 999);
-  var from = dateFrom.getTime() / 1000;
-  var to = dateTo.getTime() / 1000;
-  return $scope.detailedWeatherData.filter(function (element) {
-    return element.dt >= from && element.dt < to;
-  });
-}; */
